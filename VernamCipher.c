116747cfefb8079d4bc108433e616cff91dc5b6a
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2025-02-13 10:45:47 +0530

    Update VernamCipher.c

--- VernamCipher.c
+++ VernamCipher.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-void vernamCipher(char *text, char *key) {
+void vernamCipher(char *text, char *key) 
+{
     int i;
     int length = strlen(text);
-    for (i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) 
+    {
         text[i] = text[i] ^ key[i];
     }
 }
 
-int main() {
+int main() 
+{
     char plaintext[100], key[100];
     printf("Enter the plaintext: ");
     fgets(plaintext, sizeof(plaintext), stdin);
     size_t length = strlen(plaintext);
-    if (plaintext[length - 1] == '\n') {
+    if (plaintext[length - 1] == '\n') 
+    {
         plaintext[length - 1] = '\0';
     }
 
     printf("Enter the key (must be of the same length as plaintext): ");
     fgets(key, sizeof(key), stdin);
     length = strlen(key);
-    if (key[length - 1] == '\n') {
+    if (key[length - 1] == '\n') 
+    {
         key[length - 1] = '\0';
     }
-    if (strlen(plaintext) != strlen(key)) {
+    if (strlen(plaintext) != strlen(key)) 
+    {
         printf("Error: Key must be the same length as the plaintext.\n");
         return 1;
     }
