c1b636e950d5c955369a2566f13abff0dfd63263
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2025-03-06 10:54:18 +0530

    Add files via upload

---
+++ DES.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import javax.crypto.*;
+import java.io.*;
+import java.security.*;
+import java.security.NoSuchAlgorithmException;
+class DES
+{
+	public static void main(String args[]) throws Exception
+	{
+		String message = "kill him tomorrow.";
+		byte[] myMessage = message.getBytes();
+		
+		KeyGenerator Mygenerator = KeyGenerator.getInstance("DES");
+		SecretKey myDesKey = Mygenerator.generateKey();
+		
+		Cipher myCipher = Cipher.getInstance("DES");
+		
+		myCipher.init(Cipher.ENCRYPT_MODE,myDesKey);
+		byte[] myEncryptedBytes = myCipher.doFinal(myMessage);
+		
+		myCipher.init(Cipher.DECRYPT_MODE,myDesKey);
+		byte[] myDecryptedBytes = myCipher.doFinal(myEncryptedBytes);
+		
+		String encrypteddata = new String(myEncryptedBytes);
+		String decrypteddata = new String(myDecryptedBytes);
+		
+		System.out.println("Message : "+message);
+		System.out.println("Encrypted - "+encrypteddata);
+		System.out.println("Decrypted - "+decrypteddata);
+	}
+}
